124f5f9819777e47c94126dc85141207487d723b
PK Banks <pkbanks12@gmail.com>
2019-02-15 21:21:08 -0500

    emails and stylesheets

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => Rails.application.credentials.sendgrid[:USERNAME],
+  :password => Rails.application.credentials.sendgrid[:PASSWORD],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+}
